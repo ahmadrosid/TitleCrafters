32dd99d8c758508699cd6dd720b2d5b0048d337a
ahmadrosid <alahmadrosid@gmail.com>
2023-07-30 16:03:35 +0700

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ export default function App() {
 
   return (
     <div className="bg-gray-50 min-h-screen">
-      <div className="flex">
+      <div className="flex min-h-[90dvh]">
         <div className="w-full max-w-[260px] p-4 space-y-4">
           <h1 className="text-3xl font-bold">Title Crafters</h1>
           <a
@@ -193,6 +193,30 @@ export default function App() {
             <TableTitle data={results} />
           </div>
         </div>
+      </div>
+
+      <div className="p-6">
+        Built by{" "}
+        <a
+          className="underline text-gray-900 hover:text-gray-950"
+          href="https://ahmadrosid.com"
+        >
+          ahmadrosid
+        </a>
+        . Hosted on{" "}
+        <a
+          className="underline text-gray-900 hover:text-gray-950"
+          href="https://vercel.com"
+        >
+          Vercel.
+        </a>{" "}
+        The source code is available on{" "}
+        <a
+          className="underline text-gray-900 hover:text-gray-950"
+          href="https://github.com/ahmadrosid/TitleCrafters"
+        >
+          GitHub.
+        </a>
       </div>
     </div>
   );
